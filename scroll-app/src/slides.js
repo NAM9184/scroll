281e366026951bf4a768e86765634802b2319340
ruelline <ruelline91@naver.com>
2025-04-22 17:09:16 +0900

    이미지 교체

--- scroll-app/src/slides.js
+++ scroll-app/src/slides.js
@@ -3,31 +3,21 @@ const slides = [
     title: "Desert Oasis Pool",
     image:
       "https://images.unsplash.com/photo-1542401886-65d6c61db217?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    url: "/projects/desert-oasis",
   },
   {
     title: "Domed Sanctuary",
     image:
       "https://images.unsplash.com/photo-1593179507500-3c8ad1b2566c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    url: "/projects/domed-sanctuary",
   },
   {
-    title: "Domed Sanctuary",
+    title: "Galaxy",
     image:
-      "https://images.unsplash.com/photo-1593179507500-3c8ad1b2566c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    url: "/projects/domed-sanctuary",
+      "https://images.unsplash.com/photo-1464802686167-b939a6910659?q=80&w=2050&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    title: "Domed Sanctuary",
+    title: "Mystic Forest",
     image:
-      "https://images.unsplash.com/photo-1593179507500-3c8ad1b2566c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    url: "/projects/domed-sanctuary",
-  },
-  {
-    title: "Domed Sanctuary",
-    image:
-      "https://images.unsplash.com/photo-1593179507500-3c8ad1b2566c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    url: "/projects/domed-sanctuary",
+      "https://images.unsplash.com/photo-1561688784-2cbd459693de?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
