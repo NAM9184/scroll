2ab9c7f9777aca53e70975f70b7f06b6b39108b2
ruelline <ruelline91@naver.com>
2025-04-22 16:59:27 +0900

    포폴용 실습 완료료

---
+++ scroll-app/src/slides.js
@@ -0,0 +1,34 @@
+const slides = [
+  {
+    title: "Desert Oasis Pool",
+    image:
+      "https://images.unsplash.com/photo-1542401886-65d6c61db217?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "/projects/desert-oasis",
+  },
+  {
+    title: "Domed Sanctuary",
+    image:
+      "https://images.unsplash.com/photo-1593179507500-3c8ad1b2566c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "/projects/domed-sanctuary",
+  },
+  {
+    title: "Domed Sanctuary",
+    image:
+      "https://images.unsplash.com/photo-1593179507500-3c8ad1b2566c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "/projects/domed-sanctuary",
+  },
+  {
+    title: "Domed Sanctuary",
+    image:
+      "https://images.unsplash.com/photo-1593179507500-3c8ad1b2566c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "/projects/domed-sanctuary",
+  },
+  {
+    title: "Domed Sanctuary",
+    image:
+      "https://images.unsplash.com/photo-1593179507500-3c8ad1b2566c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "/projects/domed-sanctuary",
+  },
+];
+
+export default slides;
